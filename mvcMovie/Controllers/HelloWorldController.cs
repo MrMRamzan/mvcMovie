279021b592a01823748df6a19b52ff18a181e0e4
MuhammadRamzan54 <30586875+MuhammadRamzan54@users.noreply.github.com>
2017-11-26 00:59:48 -0800

    ASP.Net WebApp
    ASP.Net WebApp

---
+++ mvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        //public string Index()
+        //{
+        //    return "This is my <b>defult</b> action...";
+        //}
+        public string welcome(string name, int numTime=1)
+        {
+            return HttpUtility.HtmlEncode( "Hello: " +name+",ID: "+numTime);
+
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
